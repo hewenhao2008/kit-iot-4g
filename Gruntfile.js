3e71bf2ecfbe4cb04de7e70fd25f48532e07c425
Vitor Leal <vitorleal1@gmail.com>
2014-09-30 20:51:34 -0300

    remove unused modules fron gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,6 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    less: {
-      production: {
-        options: {
-          paths: ['web/css', 'web/css/less', 'web/css/common'],
-          cleancss: true
-        },
-        files: {
-          'web/css/main.css': 'web/css/main.less'
-        }
-      }
-    },
     concat: {
       options : {
         banner: '#!/usr/bin/env node\n\n'
@@ -19,22 +8,6 @@ module.exports = function (grunt) {
       bin: {
         src: 'index.js',
         dest: 'bin/index.js'
-      }
-    },
-    uglify: {
-      options: {
-        mangle: false
-      },
-      main: {
-        files: {
-          'web/js/min/app.min.js': [
-            '!web/js/min/*',
-            'web/js/app.js',
-            'web/js/controllers/*.js',
-            'web/js/services/*.js',
-            'web/js/directives/*.js'
-          ]
-        }
       }
     },
     replace: {
@@ -52,19 +25,17 @@ module.exports = function (grunt) {
     },
     watch: {
       scripts: {
-        files: ['Gruntfile.js', 'web/**/*.js', '!web/js/min/*', 'web/**/*.less'],
-        tasks: ['less', 'concat', 'uglify', 'replace']
+        files: ['Gruntfile.js'],
+        tasks: ['concat', 'replace']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('compile', ['less', 'concat', 'uglify', 'replace']);
+  grunt.registerTask('compile', ['concat', 'replace']);
 };
